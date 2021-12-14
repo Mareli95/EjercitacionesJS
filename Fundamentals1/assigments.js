0e3f858318515f954d36aed184acae044ad8f622
Mareli95 <mareli.rubio.izaguirre@gmail.com>
2021-12-14 17:43:17 -0600

    Añadiendo archivos del día 13 de diciembre

---
+++ Fundamentals1/assigments.js
@@ -0,0 +1,19 @@
+// Values and variables
+console.log('--------');
+console.log('Assigments challenge');
+console.log('--------');
+let country = 'México';
+let continent = 'América';
+let population = 1000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+// Data Types
+let isIsland = false;
+
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
